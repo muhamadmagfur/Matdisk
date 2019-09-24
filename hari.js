c6b75022f21c66079d0ad6630a6a2929bd5fad09
Muhamad Magfur <54829786+muhamadmagfur@users.noreply.github.com>
2019-09-24 12:16:15 +0700

    Add files via upload

---
+++ hari.js
@@ -0,0 +1,25 @@
+var angka = new Date().getDay();
+
+switch (angka){
+    case 0:
+        console.log("Minggu");
+        break;
+    case 1:
+        console.log("Senin");
+        break;
+    case 2:
+        console.log("Selasa");
+        break;
+    case 3:
+        console.log("Rabu");
+        break;
+    case 4:
+        console.log("Kamis");
+        break;
+    case 5:
+        console.log("Jumat");
+        break;
+    case 6:
+        console.log("Sabtu");
+        break;
+}
